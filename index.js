8626e7e8a02c1b99fa6837ff7b28b7043a1dc3cf
Khushi Patel <138273454+khupatel@users.noreply.github.com>
2024-10-11 15:44:32 -0400

    webpage creation

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+
+    // // Your web app's Firebase configuration
+    // const firebaseConfig = {
+    //     apiKey: "AIzaSyDVxKbhPpruILcuIBXE5K0FFZsVcieTH-s",
+    //     authDomain: "cmpsc487-assignment1.firebaseapp.com",
+    //     projectId: "cmpsc487-assignment1",
+    //     storageBucket: "cmpsc487-assignment1.appspot.com",
+    //     messagingSenderId: "1085370581836",
+    //     appId: "1:1085370581836:web:e56f95ca5e66c1a5ce85e9"
+    //   };
+
+
