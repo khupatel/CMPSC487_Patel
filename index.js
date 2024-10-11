b587b33c0e3234fd2307ab811732394aa6069b06
Khushi Patel <138273454+khupatel@users.noreply.github.com>
2024-10-11 18:13:28 -0400

    webpage data retrieval success

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDVxKbhPpruILcuIBXE5K0FFZsVcieTH-s",
+  authDomain: "cmpsc487-assignment1.firebaseapp.com",
+  databaseURL: "https://cmpsc487-assignment1-default-rtdb.firebaseio.com",
+  projectId: "cmpsc487-assignment1",
+  storageBucket: "cmpsc487-assignment1.appspot.com",
+  messagingSenderId: "1085370581836",
+  appId: "1:1085370581836:web:e56f95ca5e66c1a5ce85e9"
+};
 
+firebase.initializeApp(firebaseConfig);
 
+var ReservationDB = firebase.database().ref("Reservations")
 
-    // // Your web app's Firebase configuration
-    // const firebaseConfig = {
-    //     apiKey: "AIzaSyDVxKbhPpruILcuIBXE5K0FFZsVcieTH-s",
-    //     authDomain: "cmpsc487-assignment1.firebaseapp.com",
-    //     projectId: "cmpsc487-assignment1",
-    //     storageBucket: "cmpsc487-assignment1.appspot.com",
-    //     messagingSenderId: "1085370581836",
-    //     appId: "1:1085370581836:web:e56f95ca5e66c1a5ce85e9"
-    //   };
+document.getElementById('reservationForm').addEventListener('submit', submitForm)
 
+function submitForm(e) {
+  e.preventDefault(); 
+
+  var name = document.getElementById('name').value
+  var vehicletype = document.getElementById('vehicletype').value
+  var pickup = document.getElementById('pickup').value
+  var dropoff = document.getElementById('dropoff').value
+  var extension = document.getElementById('extension').value
+
+// console.log(name, vehicletype, pickup, dropoff, extension); 
+}
 
